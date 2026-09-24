199bee8da7624fb38556ddd13bc76f01f06961b8
Vubienn125 <vubien2512@gmail.com>
2026-09-24 19:27:20 +0700

    Buoi02 Bai03 Tai khoan ngan hang

--- Bai03_TaiKhoanNganHang.kt
+++ Bai03_TaiKhoanNganHang.kt
@@ -2,3 +2,19 @@
 
 package com.ute.baitapthuchanhbuoi2
 
+fun main() {
+    // val vi so du ban dau ko thay doi
+    val soDuBanDau: Double = 5_000_000.0
+
+    // var vi so du hien tai thay doi sau moi giao dich
+    var soDuHienTai: Double = soDuBanDau
+
+    // Gui them 2 trieu
+    soDuHienTai = soDuHienTai + 2_000_000.0
+    println("Sau khi gui: $soDuHienTai VND")
+
+    // Rut 1 trieu ruoi
+    soDuHienTai = soDuHienTai - 1_500_000.0
+    println("Sau khi rut: $soDuHienTai VND")
+}
+
