305c2a1e846bff75d4a2a8338b7fe106969274d2
Vubienn125 <vubien2512@gmail.com>
2026-09-24 19:57:48 +0700

    Buoi02 Bai07 Dem nguoc va lap co dinh

---
+++ Bai07_WhileVaRepeat.kt
@@ -0,0 +1,20 @@
+// 25810007_VuDinhBien
+
+package com.ute.baitapthuchanhbuoi2
+
+fun main() {
+
+    // Dung while khi so lan lap phu thuoc vao mot dieu kien
+    var so: Int = 10
+
+    while (so >= 1) {
+        println(so)
+        so--
+    }
+
+
+    // Dung repeat khi biet truoc so lan lap.
+    repeat(5) {
+        println("Truong Dai hoc Cong nghe Ky thuat TP.HCM - HCMUTE")
+    }
+}
