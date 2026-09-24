85c9a4ad34acc4919c57ed305f667b9fd639a8ae
Vubienn125 <vubien2512@gmail.com>
2026-09-24 19:15:06 +0700

    Buoi02 Bai02 Gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,11 @@
+// 25810007_VuDinhBien
+
+fun main() {
+    val hoTen = "Vu Dinh Bien"
+    val namSinh = 2000
+    val namHienTai = 2026
+
+    val tuoi = namHienTai - namSinh
+
+    println("Hi, minh ten la $hoTen, nam nay minh $tuoi tuoi. \nHien tai, minh dang hoc nganh CNTT tai HCMUTE. \nRat vui duoc lam quen voi moi nguoi")
+}
