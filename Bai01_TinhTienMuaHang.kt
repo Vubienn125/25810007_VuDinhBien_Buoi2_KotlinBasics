9d5994eafb12d4e78c6205e64491f4713125a826
Vubienn125 <vubien2512@gmail.com>
2026-09-24 18:53:31 +0700

    Buoi02 Bai1 Tinh tien mua hang

---
+++ Bai01_TinhTienMuaHang.kt
@@ -0,0 +1,14 @@
+package com.ute.baitapthuchanhbuoi2
+
+fun main() {
+    val soLuong = 5
+    val donGia = 25000.0
+
+    val tienHang = soLuong.toDouble() * donGia
+    val thue = tienHang * 0.08
+    val tongTien = tienHang + thue
+
+    println("Tien hang: $tienHang VND")
+    println("Thue: $thue VND")
+    println("Tong tien cuoi cung: $tongTien VND")
+}
